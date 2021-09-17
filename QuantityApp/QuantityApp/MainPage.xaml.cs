0811f27d683b91ea9a0239844a4d7ae0d7df910f
Leonardo Rocha <leonardo@eosconsultores.com.br>
2021-09-16 23:31:11 -0400

    Add project files.

---
+++ QuantityApp/QuantityApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace QuantityApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int quantidade = 1;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Adicao(object sender, EventArgs e)
+        {
+            quantidade++;
+            LblQuantidade.Text = quantidade.ToString();
+        }
+
+        private void Subtrair(object sender, EventArgs e)
+        {
+            quantidade--;
+            LblQuantidade.Text = quantidade.ToString();
+        }
+    }
+}
